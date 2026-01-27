d8ab2a484956dbf0ac904c6053674abad8f6e52f
Chezel Aguilar <aguilarchezel25@gmail.com>
2026-01-27 15:40:17 +0800

    app.listen

--- backend/server.js
+++ backend/server.js
@@ -3005,5 +3005,5 @@ app.post('/api/admin/reset-password', async (req, res) => {
 });
 
 // ---------------- SERVER ----------------
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`✅ Server running on http://localhost:${PORT}`));
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
